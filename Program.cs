a006254d9d45e56c96253e845f0ead32c9f88323
Abriansyah Adam <abriansyah.adam.ft21@mail.umy.ac.id>
2022-11-14 16:53:01 +0700

    ...

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
 ﻿namespace LinkedList
 {
-    internal class Program
+    class Node
+    {
+        /*Node class reperesents the node of doubly linked list.
+          it consist of the information part and links to
+          it's succeedind and precceeding
+          in terms of next and previous*/
+        public int noMhs;
+        public string name;
+        // poinnt to the succeeding node
+        public Node next;
+        // poinnt to the precceeding node
+        public Node prev;
+
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+
+        // constructor
+
+    }
+    class Program
     {
         static void Main(string[] args)
         {
